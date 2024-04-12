bc6c276a3819198fe6276f61509401ac6fd44c6a
KyungMinKim <kmkimworkwork@gmail.com>
2024-04-12 17:28:59 +0900

    [jdbcTemplate] getByUserName 작성 및 jdbcTemplate DataSource 적용 확인

--- src/main/java/com/example/simplespringboot/SimpleSpringBootApplication.java
+++ src/main/java/com/example/simplespringboot/SimpleSpringBootApplication.java
@@ -1,7 +1,9 @@
 package com.example.simplespringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class SimpleSpringBootApplication {
